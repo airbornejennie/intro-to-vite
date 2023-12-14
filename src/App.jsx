5105ebed0b4a8f5ca3be6a27c723fbc9409f12fd
airbornejennie <146116778+airbornejennie@users.noreply.github.com>
2023-12-13 22:01:30 -0600

    updated app.css and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,37 @@ console.log(puppyList);
 function App() {
   // const [count, setCount] = useState(0)
   const [puppies, setPuppies] = useState(puppyList);
-
+  const [featPupId, setFeatPupId] = useState(null);
   console.log("puppyList: ", puppyList);
+  // function handleClick() {
+  //   // some logic here
+  // }
+  
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <>
       <div className="App">
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+        {puppies.map((puppy) => {
+          return (
+          <p onClick={() => {
+            setFeatPupId(puppy.id)
+          }} key={puppy.id}>{puppy.name}
+          </p>
+          );
+        })}
       </div>
-      {/* <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+      {featPupId && (
+        <div id="feature">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </>  
   );
 }
 
